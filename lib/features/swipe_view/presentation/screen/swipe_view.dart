3f26fc3a3527fbe4cfffe9e63dadebcf3c901142
sm-amoled <moktak072@naver.com>
2023-11-04 17:52:06 +0900

    [#51] feat: carousel ui 추가

--- lib/features/swipe_view/presentation/screen/swipe_view.dart
+++ lib/features/swipe_view/presentation/screen/swipe_view.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wehavit/features/swipe_view/data/enitty/DEBUG_confirm_post_model.dart';
@@ -15,6 +19,9 @@ class SwipeView extends ConsumerStatefulWidget {
 }
 
 class SwipeViewState extends ConsumerState<SwipeView> {
+  int _currentCellNumber = 0;
+  final CarouselController _carouselController = CarouselController();
+
   @override
   Widget build(BuildContext context) {
     var swipeViewProviderList = ref.watch(swipeViewProvider);
@@ -29,64 +36,88 @@ class SwipeViewState extends ConsumerState<SwipeView> {
           (failure) => Container(
             color: Colors.cyan,
           ),
-          (modelList) => Container(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Container(
-                constraints: BoxConstraints.expand(),
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Stack(
-                        children: List<Widget>.generate(
-                          modelList.length,
-                          (index) {
-                            return Container(
-                              child: Column(
-                                children: [
-                                  Container(
-                                    height: 20,
-                                    color: Colors.blueGrey,
-                                  ),
-                                  SwipeViewCellWidget(
-                                    model: modelList[index],
-                                  ),
-                                  Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Expanded(child: Container()),
-                                          ElevatedButton.icon(
-                                            onPressed: () {},
-                                            icon: Icon(Icons.emoji_emotions),
-                                            label: Text(""),
-                                          ),
-                                          SizedBox(
-                                            width: 10,
-                                          ),
-                                          ElevatedButton.icon(
-                                            onPressed: () {},
-                                            icon: Icon(Icons.emoji_emotions),
-                                            label: Text(""),
-                                          ),
-                                        ],
-                                      ),
-                                      TextFormField(
-                                        decoration: InputDecoration(
-                                            border: OutlineInputBorder()),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            );
-                          },
+          (modelList) => Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  height: 10,
+                  color: Colors.blueGrey,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List<Widget>.generate(
+                      modelList.length,
+                      (index) => Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                          child: Container(
+                            key: ValueKey(index),
+                            // padding: EdgeInsets.symmetric(horizontal: 15),
+                            decoration: BoxDecoration(
+                                color: _currentCellNumber == index
+                                    ? Colors.amber
+                                    : Colors.grey[400]),
+                            height: 8,
+                          ),
                         ),
                       ),
                     ),
+                  ),
+                ),
+                Expanded(
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                        viewportFraction: 1.0,
+                        height: MediaQuery.of(context).size.height,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _currentCellNumber = index;
+                          });
+                        },
+                        enableInfiniteScroll: false),
+                    carouselController: _carouselController,
+                    items: List<Widget>.generate(
+                      modelList.length,
+                      (index) {
+                        return Flex(
+                          direction: Axis.vertical,
+                          children: [
+                            SwipeViewCellWidget(
+                              model: modelList[index],
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(child: Container()),
+                        ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.emoji_emotions),
+                          label: Text(""),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.emoji_emotions),
+                          label: Text(""),
+                        ),
+                      ],
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                    ),
                   ],
                 ),
-              ),
+              ],
             ),
           ),
         ),
