eec2876152af8537c215364bec5de98b7b86d8a8
Juhyeong Ko <dury.ko@gmail.com>
2023-11-15 08:06:36 +0900

    [#90] feat: add sync live writing

---
+++ lib/features/live_writing/domain/repositories/live_confirm_post_repository_provider.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'live_confirm_post_repository_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$liveConfirmPostRepositoryHash() =>
+    r'48e9b5b3b1bf68dd1a56627109a758145ea3482c';
+
+/// See also [liveConfirmPostRepository].
+@ProviderFor(liveConfirmPostRepository)
+final liveConfirmPostRepositoryProvider =
+    AutoDisposeProvider<LiveConfirmPostRepository>.internal(
+  liveConfirmPostRepository,
+  name: r'liveConfirmPostRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$liveConfirmPostRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LiveConfirmPostRepositoryRef
+    = AutoDisposeProviderRef<LiveConfirmPostRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
