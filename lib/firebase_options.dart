a81c01d32f942484b9b9f604d48d3b35615af2a9
Juhyeong Ko <dury.ko@gmail.com>
2023-11-28 02:25:42 +0900

    [#107] fix: build settings
    fixed android permission
    updated firebase options

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,24 +17,15 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for android - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return android;
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return macos;
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -52,6 +43,26 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyC9ZxMvb21D_3Jdf0FzlG8UsmrlL2gDacY',
+    appId: '1:764897522660:web:ddd978b027d2fff6b1cdf1',
+    messagingSenderId: '764897522660',
+    projectId: 'we-havit-be',
+    authDomain: 'we-havit-be.firebaseapp.com',
+    databaseURL: 'https://we-havit-be-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'we-havit-be.appspot.com',
+    measurementId: 'G-707T7ETWGP',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyCE5n-Nv6H8SFri1wJBMOeLZ_AXEFq_-Y4',
+    appId: '1:764897522660:android:39b6b251ac1924feb1cdf1',
+    messagingSenderId: '764897522660',
+    projectId: 'we-havit-be',
+    databaseURL: 'https://we-havit-be-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'we-havit-be.appspot.com',
+  );
+
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyCpEgnUEQs27KjI8pJaypqaTlYzmRkhIa4',
     appId: '1:764897522660:ios:b0a95f48091a963db1cdf1',
@@ -62,5 +73,17 @@ class DefaultFirebaseOptions {
     androidClientId: '764897522660-88jtfk0rhihjcduu4dfjqm0a0nuqdlei.apps.googleusercontent.com',
     iosClientId: '764897522660-njq584kfil0otf92p4ujml8b6b5pegj9.apps.googleusercontent.com',
     iosBundleId: 'com.bootcamp.wehavit',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyCpEgnUEQs27KjI8pJaypqaTlYzmRkhIa4',
+    appId: '1:764897522660:ios:48a083e63cc55893b1cdf1',
+    messagingSenderId: '764897522660',
+    projectId: 'we-havit-be',
+    databaseURL: 'https://we-havit-be-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'we-havit-be.appspot.com',
+    androidClientId: '764897522660-88jtfk0rhihjcduu4dfjqm0a0nuqdlei.apps.googleusercontent.com',
+    iosClientId: '764897522660-ollgi4mmng0b687sussgeu9hpjd4891d.apps.googleusercontent.com',
+    iosBundleId: 'com.bootcamp.wehavit.RunnerTests',
   );
 }
