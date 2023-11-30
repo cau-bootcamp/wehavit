8cf10883859144c13c910585a7b2c83401e0f478
sm-amoled <moktak072@naver.com>
2023-11-30 21:34:40 +0900

    [#112] feat: LiveWritingView 친구 bubble UI 작성

--- lib/features/live_writing/presentation/screens/live_writing_view.dart
+++ lib/features/live_writing/presentation/screens/live_writing_view.dart
@@ -12,15 +12,15 @@ class LiveWritingView extends StatelessWidget {
         elevation: 0,
       ),
       body: SafeArea(
-        minimum: EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                child: FriendLiveBubbleWidget(),
-              ),
+        minimum: const EdgeInsets.all(16.0),
+        child: Column(
+          children: List<Widget>.generate(
+            4,
+            (index) => Padding(
+              padding: EdgeInsets.symmetric(vertical: 12.0),
+              child: FriendLiveBubbleWidget(),
             ),
-          ],
+          ),
         ),
       ),
     );
@@ -39,238 +39,322 @@ class _FriendLiveBubbleWidgetState extends State<FriendLiveBubbleWidget> {
   LiveBubbleState bubbleState = LiveBubbleState.showingDefault;
 
   final double profileImageRadius = 23;
+  final String userName = 'real_buddah';
+  final String postTitle = '하루에 한 번 웃기 2일차';
+  final String postContent =
+      '사리자여... 몇번을 말해야 알아듣느냐... 오늘도 무사히 세번을 참아 냈다. 아재아재 바라아제 바라승아제 모지사바하';
+  final ImageProvider userImage = NetworkImage(
+    'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
+  );
+  final ImageProvider postImage = NetworkImage(
+    'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
+  );
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Visibility(
         visible: writingState != LiveWritingState.writing,
-        replacement: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20.0),
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(right: 8.0),
-                child: CircleAvatar(
-                  radius: 35,
-                  foregroundImage: NetworkImage(
-                    'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 46,
-                bottom: 50,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xaa8C8C8C),
-                    borderRadius: BorderRadius.circular(50.0),
-                  ),
-                  width: 77,
-                  height: 35,
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(
-                        3,
-                        (_) => Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: Container(
-                            decoration: const BoxDecoration(
-                                shape: BoxShape.circle, color: Colors.black),
-                            width: 8,
-                            height: 8,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+        replacement: WaitingStateUserBubble(userImage: userImage),
         child: Row(
           mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: EdgeInsets.only(right: 8.0),
               child: CircleAvatar(
                 radius: profileImageRadius,
-                foregroundImage: NetworkImage(
-                  'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
-                ),
+                foregroundImage: userImage,
               ),
             ),
             Expanded(
               child: Container(
-                height: 47,
+                height:
+                    bubbleState == LiveBubbleState.showingDefault ? 47 : null,
                 child: LayoutBuilder(
                   builder: (BuildContext context, BoxConstraints constraints) {
-                    print(constraints.maxWidth);
-                    return Container(
-                      child: Stack(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10.0),
-                              ),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Flexible(
-                                    child: Container(
-                                      constraints:
-                                          BoxConstraints(minWidth: 120),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(2.0),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              "Name",
-                                              overflow: TextOverflow.ellipsis,
-                                              maxLines: 1,
-                                            ),
-                                            Text(
-                                              "도전 목표",
-                                              overflow: TextOverflow.ellipsis,
-                                              maxLines: 1,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                          Radius.circular(10.0),
-                                        ),
-                                        boxShadow: [
-                                          BoxShadow(
-                                              color: Colors.black26,
-                                              spreadRadius: 2,
-                                              blurRadius: 2,
-                                              offset: Offset(0, 2))
-                                        ]),
-                                    clipBehavior: Clip.hardEdge,
-                                    child: Image(
-                                      width: 50,
-                                      height: 37,
-                                      fit: BoxFit.fill,
-                                      image: NetworkImage(
-                                        'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
+                    return GestureDetector(
+                      onTapUp: (details) {
+                        setState(() {
+                          bubbleState =
+                              bubbleState == LiveBubbleState.showingDefault
+                                  ? LiveBubbleState.showingDetail
+                                  : LiveBubbleState.showingDefault;
+                        });
+                      },
+                      child: LiveUserBubble(
+                        userName: userName,
+                        postTitle: postTitle,
+                        bubbleState: bubbleState,
+                        postContent: postContent,
+                        postImage: postImage,
                       ),
                     );
-                    // print(constraints.maxWidth);
-                    // if (constraints.maxWidth > 200) {
-                    //   return Container(
-                    //     decoration: BoxDecoration(color: Colors.black26),
-                    //     child: Text('BIGfjdkalf;jdksla;fjdkfdfdsjasaj'),
-                    //   );
-                    // } else {
-                    //   return Text('SMALL');
-                    // }
                   },
                 ),
               ),
             ),
-            // Expanded(
-            //   child: Row(
-            //     mainAxisSize: MainAxisSize.min,
-            //     children: [
-            //       Expanded(
-            //         child: Text(
-            //           "fdjsalffdsa;fdsafdsafdafdsa",
-            //         ),
-            //       ),
-            //       Padding(
-            //         padding: EdgeInsets.all(5.0),
-            //         child: Image(
-            //           width: 50,
-            //           height: 37,
-            //           fit: BoxFit.fill,
-            //           image: NetworkImage(
-            //             'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
-            //           ),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-
-            // Container(
-            //   constraints: BoxConstraints.expand(),
-            //   height: 47,
-            // ),
-
-            // child: Row(
-            //   children: [
-            //     Container(
-            //       constraints: BoxConstraints(
-            //         minHeight: 47,
-            //       ),
-            //       child: Container(
-            //         color: Colors.black45,
-            //         child: Row(
-            //           children: [
-            //             Padding(
-            //               padding: EdgeInsets.all(8.0),
-            //               child: Container(
-            //                 height: 47,
-            //                 child: Column(
-            //                   crossAxisAlignment: CrossAxisAlignment.start,
-            //                   mainAxisAlignment: MainAxisAlignment.center,
-            //                   children: [
-            //                     Container(
-            //                       child: Text(
-            //                         "이름",
-            //                       ),
-            //                     ),
-            //                     Text(
-            //                       // maxLines: 1,
-            //                       overflow: TextOverflow.ellipsis,
-            //                       "도전 목fdsfsadfdfdfffffdfdsafdsafdsasafafdsafdaafa표",
-            //                     ),
-            //                   ],
-            //                 ),
-            //               ),
-            //             ),
-            //             Padding(
-            //               padding: const EdgeInsets.all(5.0),
-            //               child: Image(
-            //                 width: 50,
-            //                 height: 37,
-            //                 fit: BoxFit.fill,
-            //                 image: NetworkImage(
-            //                   'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
-            //                 ),
-            //               ),
-            //             ),
-            //           ],
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class LiveUserBubble extends StatelessWidget {
+  const LiveUserBubble({
+    super.key,
+    required this.userName,
+    required this.postTitle,
+    required this.bubbleState,
+    required this.postContent,
+    required this.postImage,
+  });
+
+  final String userName;
+  final String postTitle;
+  final LiveBubbleState bubbleState;
+  final String postContent;
+  final ImageProvider<Object> postImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      child: Container(
+                        constraints: const BoxConstraints(minWidth: 120),
+                        child: Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              PostTitleWidget(userName: userName),
+                              PostContentWidget(postTitle: postTitle),
+                              PostImageWidgetForDefault(
+                                bubbleState: bubbleState,
+                                postContent: postContent,
+                                postImage: postImage,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    PostWidgetImageForDetail(
+                      bubbleState: bubbleState,
+                      postImage: postImage,
+                    ),
+                  ],
+                ),
+                // TODO : 반응 남기기 기능 개발
+                Visibility(
+                  visible: bubbleState == LiveBubbleState.showingDetail,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: List<Widget>.generate(
+                      6,
+                      (index) => const Placeholder(
+                        fallbackWidth: 40,
+                        fallbackHeight: 40,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PostContentWidget extends StatelessWidget {
+  const PostContentWidget({
+    super.key,
+    required this.postTitle,
+  });
+
+  final String postTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      postTitle,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 1,
+    );
+  }
+}
+
+class PostTitleWidget extends StatelessWidget {
+  const PostTitleWidget({
+    super.key,
+    required this.userName,
+  });
+
+  final String userName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      userName,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 1,
+    );
+  }
+}
+
+class PostWidgetImageForDetail extends StatelessWidget {
+  const PostWidgetImageForDetail({
+    super.key,
+    required this.bubbleState,
+    required this.postImage,
+  });
+
+  final LiveBubbleState bubbleState;
+  final ImageProvider<Object> postImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: bubbleState == LiveBubbleState.showingDefault,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 4.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  spreadRadius: 2,
+                  blurRadius: 2,
+                  offset: Offset(0, 2))
+            ]),
+        clipBehavior: Clip.hardEdge,
+        child: Image(
+          width: 50,
+          height: 37,
+          fit: BoxFit.fill,
+          image: postImage,
+        ),
+      ),
+    );
+  }
+}
+
+class PostImageWidgetForDefault extends StatelessWidget {
+  const PostImageWidgetForDefault({
+    super.key,
+    required this.bubbleState,
+    required this.postContent,
+    required this.postImage,
+  });
+
+  final LiveBubbleState bubbleState;
+  final String postContent;
+  final ImageProvider<Object> postImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: bubbleState == LiveBubbleState.showingDetail,
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                postContent,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 4,
+          ),
+          Visibility(
+            visible: bubbleState != LiveBubbleState.showingDefault,
+            child: Image(
+                width: 113, height: 84, fit: BoxFit.fill, image: postImage),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WaitingStateUserBubble extends StatelessWidget {
+  const WaitingStateUserBubble({
+    super.key,
+    required this.userImage,
+  });
+
+  final ImageProvider<Object> userImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: CircleAvatar(
+              radius: 35,
+              foregroundImage: userImage,
+            ),
+          ),
+          Positioned(
+            left: 46,
+            bottom: 50,
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xaa8C8C8C),
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+              width: 77,
+              height: 35,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                    3,
+                    (_) => Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black,
+                        ),
+                        width: 8,
+                        height: 8,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
