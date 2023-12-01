14cb969293ac12a380cfc8a135edf258d5271e10
sm-amoled <moktak072@naver.com>
2023-12-01 10:23:26 +0900

    [#112] feat: 시간 위젯 추가

--- lib/features/live_writing/presentation/screens/live_writing_view.dart
+++ lib/features/live_writing/presentation/screens/live_writing_view.dart
@@ -8,113 +8,149 @@ class LiveWritingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.transparent,
+      //   foregroundColor: Colors.black,
+      //   leading: IconButton(
+      //     onPressed: () {
+      //       Navigator.of(context).pop();
+      //     },
+      //     icon: Icon(Icons.abc),
+      //   ),
+      //   elevation: 0,
+      // ),
       body: SafeArea(
         minimum: const EdgeInsets.all(16.0),
-        child: Stack(
-          children: [
-            SingleChildScrollView(
-              child: Column(
-                children: List<Widget>.generate(
-                  4,
-                  (index) => Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12.0),
-                    child: FriendLivePostWidget(),
-                  ),
-                ),
-              ),
-            ),
-            Align(
-              alignment: const Alignment(1, 1),
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  return Container(
-                    color: Colors.grey.shade800,
-                    width: constraints.maxWidth,
-                    height: constraints.maxWidth * 0.84,
-                    child: Container(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Container(
+          child: Stack(
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back_ios)),
+              LayoutBuilder(builder: (context, constraints) {
+                return Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 34, bottom: 34),
+                      width: double.infinity,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Row(
-                              children: [
-                                CircleAvatar(
-                                  radius: 32,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16.0),
-                                  child: Text("Name"),
-                                ),
-                                Expanded(child: Container()),
-                                Text("도전 목표"),
-                              ],
+                            Text(
+                              "남은 시간",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w400),
                             ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 10.0),
-                              child: Text("제목"),
+                            Text(
+                              "00:07",
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
                             ),
-                            Divider(
-                              color: Colors.amber,
-                              thickness: 2.0,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                    child: TextFormField(
-                                  maxLines: 5,
-                                  style: TextStyle(color: Colors.white),
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                  ),
-                                )),
-                                SizedBox(
-                                  width: 8,
-                                ),
-                                Visibility(
-                                    child: Container(
-                                  width: 151,
-                                  height: 104,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(
-                                    Radius.circular(10.0),
-                                  )),
-                                  child: Image(
-                                      fit: BoxFit.cover,
-                                      image: NetworkImage(
-                                          'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202306/25/488f9638-800c-4bac-ad65-82877fbff79b.jpg')),
-                                )),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Container(),
-                                ),
-                                ElevatedButton(
-                                    onPressed: () {}, child: Text("휴식")),
-                                SizedBox(
-                                  width: 12,
-                                ),
-                                ElevatedButton(
-                                    onPressed: () {}, child: Text("완료")),
-                              ],
-                            )
-                          ],
+                          ]),
+                    ),
+                    SingleChildScrollView(
+                      padding:
+                          EdgeInsets.only(bottom: constraints.maxWidth * 0.84),
+                      child: Column(
+                        children: List<Widget>.generate(
+                          4,
+                          (index) => Padding(
+                            padding: EdgeInsets.symmetric(vertical: 12.0),
+                            child: FriendLivePostWidget(),
+                          ),
                         ),
                       ),
                     ),
-                  );
-                },
+                  ],
+                );
+              }),
+              Align(
+                alignment: const Alignment(1, 1),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    return Container(
+                      color: Colors.grey.shade800,
+                      width: constraints.maxWidth,
+                      height: constraints.maxWidth * 0.84,
+                      child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 32,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 16.0),
+                                    child: Text("Name"),
+                                  ),
+                                  Expanded(child: Container()),
+                                  Text("도전 목표"),
+                                ],
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10.0),
+                                child: Text("제목"),
+                              ),
+                              Divider(
+                                color: Colors.amber,
+                                thickness: 2.0,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                      child: TextFormField(
+                                    maxLines: 5,
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                    ),
+                                  )),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  Visibility(
+                                      child: Container(
+                                    width: 151,
+                                    height: 104,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.all(
+                                      Radius.circular(10.0),
+                                    )),
+                                    child: Image(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(
+                                            'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202306/25/488f9638-800c-4bac-ad65-82877fbff79b.jpg')),
+                                  )),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Container(),
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {}, child: Text("휴식")),
+                                  SizedBox(
+                                    width: 12,
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {}, child: Text("완료")),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
