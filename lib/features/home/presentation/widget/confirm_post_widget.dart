2443c1133e47dfa556d3d684f04e72517542c891
Tastypotato245 <rbt2775@cau.ac.kr>
2023-12-02 22:45:11 +0900

    [#96 && #97] feat: 기능 연결, but 내부 구현이 어떻게 되어있는지 몰라서, 해당 confirmPost로 데이터가 들어가는지 잘 모르겠음.

--- lib/features/home/presentation/widget/confirm_post_widget.dart
+++ lib/features/home/presentation/widget/confirm_post_widget.dart
@@ -1,18 +1,29 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wehavit/common/common.dart';
 import 'package:wehavit/features/home/domain/models/confirm_post_model.dart';
+import 'package:wehavit/features/swipe_view/presentation/model/swipe_view_model.dart';
+import 'package:wehavit/features/swipe_view/presentation/provider/swipe_view_model_provider.dart';
+import 'package:wehavit/features/swipe_view/presentation/screen/widget/emoji_sheet_widget.dart';
 
-class ConfirmPostWidget extends StatefulWidget {
+import '../../../../common/utils/emoji_assets.dart';
+
+class ConfirmPostWidget extends ConsumerStatefulWidget {
   const ConfirmPostWidget({super.key, required this.model});
 
   final ConfirmPostModel model;
 
   @override
-  State<ConfirmPostWidget> createState() => _ConfirmPostWidgetState();
+  ConsumerState<ConfirmPostWidget> createState() {
+    return _ConfirmPostWidgetState();
+  }
 }
 
-class _ConfirmPostWidgetState extends State<ConfirmPostWidget> {
+class _ConfirmPostWidgetState extends ConsumerState<ConfirmPostWidget>
+    with SingleTickerProviderStateMixin {
   late String userName = widget.model.userName;
   late String userImageUrl = widget.model.userImageUrl;
   late String resolutionGoalStatement = widget.model.resolutionGoalStatement;
@@ -20,6 +31,39 @@ class _ConfirmPostWidgetState extends State<ConfirmPostWidget> {
   late String content = widget.model.content;
   late String contentImageUrl = widget.model.contentImageUrl;
   late Timestamp postAt = widget.model.postAt;
+  late final SwipeViewModel _swipeViewModel;
+  late final SwipeViewModelProvider _swipeViewModelProvider;
+
+  bool _initOccurred = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Future<void> didChangeDependencies() async {
+    super.didChangeDependencies();
+
+    if (!_initOccurred) {
+      _swipeViewModel = ref.watch(swipeViewModelProvider);
+      _swipeViewModelProvider = ref.read(swipeViewModelProvider.notifier);
+      await ref
+          .read(swipeViewModelProvider.notifier)
+          .getTodayConfirmPostModelList();
+
+      await _swipeViewModelProvider.initializeCamera();
+
+      _swipeViewModel.cameraButtonPosition =
+          _swipeViewModelProvider.getCameraButtonPosition() ??
+              const Offset(0, 0);
+
+      setAnimationVariables();
+
+      setState(() {});
+      _initOccurred = true;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -148,25 +192,205 @@ class _ConfirmPostWidgetState extends State<ConfirmPostWidget> {
                     onPressed: () async {},
                     icon: const Icon(Icons.send),
                   ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.emoji_emotions,
+                  GestureDetector(
+                    onTapUp: (details) async =>
+                        emojiSheetWidget(context).whenComplete(() async {
+                      _swipeViewModelProvider.sendEmojiReaction();
+                    }),
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '😄',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
                     ),
-                    onPressed: () {
-                      // 기능 연결
-                    },
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.camera_alt,
-                        color: CustomColors.whYellow),
-                    onPressed: () {},
+                  Container(
+                    key: _swipeViewModel.cameraButtonPlaceholderKey,
+                    width: 50,
+                    height: 50,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      // color: Colors.black,
+                    ),
+                    child: Center(
+                      child: Text(
+                        _swipeViewModel.isCameraInitialized == true
+                            ? '📸'
+                            : '❌',
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                    ),
                   ),
+//                  IconButton(
+//                    icon: const Icon(
+//                      Icons.emoji_emotions,
+//                    ),
+//                    onPressed: () {
+//                      // 기능 연결
+//                    },
+//                  ),
+//                  IconButton(
+//                    icon: const Icon(Icons.camera_alt,
+//                        color: CustomColors.whYellow),
+//                    onPressed: () {
+//                      // 기능 연결
+//                    },
+//                  ),
                 ],
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Future<dynamic> emojiSheetWidget(BuildContext context) {
+    return showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      clipBehavior: Clip.none,
+      elevation: 0,
+      context: context,
+      builder: (context) {
+        void disposeWidget(UniqueKey key) {
+          setState(() {
+            _swipeViewModel.emojiWidgets.remove(key);
+          });
+        }
+
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Stack(
+              alignment: Alignment.center,
+              clipBehavior: Clip.none,
+              children: [
+                Stack(
+                  alignment: Alignment.topCenter,
+                  clipBehavior: Clip.none,
+                  children: _swipeViewModel.emojiWidgets.values.toList(),
+                ),
+                Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 30.0),
+                      child: Text(
+                        '반응을 ${_swipeViewModel.countSend}회 보냈어요!',
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Column(
+                        children: List<Widget>.generate(
+                          3,
+                          (index) => Row(
+                            children: List<Widget>.generate(5, (jndex) {
+                              final key = GlobalKey();
+                              return Expanded(
+                                key: key,
+                                child: GestureDetector(
+                                  onTapDown: (detail) {},
+                                  onTapUp: (detail) {
+                                    shootEmoji(
+                                      setState,
+                                      index,
+                                      jndex,
+                                      detail,
+                                      context,
+                                      disposeWidget,
+                                    );
+                                  },
+                                  child: Stack(
+                                    clipBehavior: Clip.none,
+                                    children: [
+                                      Image(
+                                        image: AssetImage(
+                                          Emojis.emojiList[index * 5 + jndex],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 60,
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void setAnimationVariables() {
+    _swipeViewModel.animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+    _swipeViewModel.animation = Tween<double>(begin: 0, end: 130).animate(
+      CurvedAnimation(
+        parent: _swipeViewModel.animationController,
+        curve: Curves.linear,
+      ),
+    );
+    _swipeViewModel.animationController.value = 1;
+  }
+
+  void shootEmoji(
+    StateSetter setState,
+    int index,
+    int jndex,
+    TapUpDetails detail,
+    BuildContext context,
+    void Function(UniqueKey key) disposeWidget,
+  ) {
+    return setState(
+      () {
+        _swipeViewModel.countSend++;
+        _swipeViewModel.sendingEmojis[index * 5 + jndex] += 1;
+        final animationWidgetKey = UniqueKey();
+        _swipeViewModel.emojiWidgets.addEntries(
+          {
+            animationWidgetKey: ShootEmojiWidget(
+              key: animationWidgetKey,
+              emojiIndex: index * 5 + jndex,
+              currentPos:
+                  Point(detail.globalPosition.dx, detail.globalPosition.dy),
+              targetPos: Point(
+                MediaQuery.of(context).size.width / 2,
+                MediaQuery.of(context).size.height + 50,
+              ),
+              disposeWidgetFromParent: disposeWidget,
+            ),
+          }.entries,
+        );
+      },
     );
   }
 }
