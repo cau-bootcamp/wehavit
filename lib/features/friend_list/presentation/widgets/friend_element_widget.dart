ad910ad6f831fa8efe90f02de45e789b136f4e8f
Tastypotato245 <rbt2775@cau.ac.kr>
2023-12-04 22:01:55 +0900

    [#123] feat: 활성화 서클 추가 (activeCircle)

--- lib/features/friend_list/presentation/widgets/friend_element_widget.dart
+++ lib/features/friend_list/presentation/widgets/friend_element_widget.dart
@@ -20,27 +20,61 @@ class _FriendElementWidgetState extends State<FriendElementWidget> {
     return Padding(
       padding: const EdgeInsets.only(left: 12.0, top: 8.0, bottom: 8.0),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            margin: const EdgeInsets.only(left: 4.0, right: 16.0),
-            child: CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(friendImageUrl),
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
             children: [
-              Text(
-                friendName,
-                style: const TextStyle(
-                  color: CustomColors.whWhite,
-                  fontWeight: FontWeight.bold,
+              Container(
+                margin: const EdgeInsets.only(left: 4.0, right: 16.0),
+                child: CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(friendImageUrl),
                 ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    friendName,
+                    style: const TextStyle(
+                      color: CustomColors.whWhite,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
+          Row(
+            children: [
+              ActiveCircle(true),
+              ActiveCircle(false),
+              ActiveCircle(false),
+              Container(
+                margin: const EdgeInsets.only(right: 16.0),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container ActiveCircle(bool isActive) {
+    return Container(
+      margin: const EdgeInsets.only(left: 4.0),
+      width: 24,
+      height: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: isActive ? CustomColors.whYellow : CustomColors.whYellowDark,
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black54,
+            blurRadius: 5.0,
+            offset: Offset(0, 2),
+          )
         ],
       ),
     );
