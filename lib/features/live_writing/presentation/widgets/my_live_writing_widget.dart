7de36e747b4919d2afd656d3fbd7211a8e6af075
sm-amoled <moktak072@naver.com>
2023-12-07 11:14:26 +0900

    [#114] feat: 내 위젯에 대한 디자인 적용

--- lib/features/live_writing/presentation/widgets/my_live_writing_widget.dart
+++ lib/features/live_writing/presentation/widgets/my_live_writing_widget.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:wehavit/common/constants/app_colors.dart';
 import 'package:wehavit/common/errors/failure.dart';
 import 'package:wehavit/common/models/user_model/user_model.dart';
 import 'package:wehavit/common/utils/custom_types.dart';
@@ -79,7 +80,10 @@ class _MyLiveWritingWidgetState extends ConsumerState<MyLiveWritingWidget> {
       child: LayoutBuilder(
         builder: (context, constraints) {
           return Container(
-            color: Colors.grey.shade800,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: CustomColors.whSemiBlack,
+            ),
             width: constraints.maxWidth,
             height: 320,
             child: Padding(
@@ -106,6 +110,7 @@ class _MyLiveWritingWidgetState extends ConsumerState<MyLiveWritingWidget> {
                         width: 151,
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
                             Padding(
                               padding: const EdgeInsets.only(
@@ -165,23 +170,48 @@ class _MyLiveWritingWidgetState extends ConsumerState<MyLiveWritingWidget> {
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Text(
             FirebaseAuth.instance.currentUser!.displayName!,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: CustomColors.whWhite,
+            ),
           ),
         ),
         Expanded(child: Container()),
-        Text(widget.resolutionModel.goalStatement),
+        Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: Text(
+            '· ${widget.resolutionModel.goalStatement}',
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: CustomColors.whYellow,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
       ],
     );
   }
 
   TextFormField titleTextFormFieldWidget(
-      TextEditingController titleController) {
+    TextEditingController titleController,
+  ) {
     return TextFormField(
       controller: titleController,
-      style: const TextStyle(color: Colors.white),
-      decoration: const InputDecoration(
+      style: const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+        color: CustomColors.whWhite,
+      ),
+      decoration: InputDecoration(
         border: InputBorder.none,
-        hintText: '제목',
-        hintStyle: TextStyle(color: Colors.white),
+        hintText: '어떤 노력을 하셨나요?',
+        hintStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          color: CustomColors.whSemiWhite.withAlpha(0xa0),
+        ),
       ),
     );
   }
@@ -191,11 +221,19 @@ class _MyLiveWritingWidgetState extends ConsumerState<MyLiveWritingWidget> {
     return TextFormField(
       controller: contentController,
       maxLines: 6,
-      style: const TextStyle(color: Colors.white),
-      decoration: const InputDecoration(
+      style: const TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: CustomColors.whWhite,
+      ),
+      decoration: InputDecoration(
         border: InputBorder.none,
-        hintText: '본문',
-        hintStyle: TextStyle(color: Colors.white),
+        hintText: '오늘의 이야기를 들려주세요',
+        hintStyle: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: CustomColors.whWhite.withAlpha(0xa0),
+        ),
       ),
     );
   }
@@ -228,12 +266,29 @@ class _MyLiveWritingWidgetState extends ConsumerState<MyLiveWritingWidget> {
           visible: imageFile != null,
           replacement: Container(
             decoration: BoxDecoration(
+              color: CustomColors.whYellowBright,
+              borderRadius: BorderRadius.circular(10.0),
               border: Border.all(
-                color: Colors.white,
+                width: 2,
+                color: CustomColors.whYellow,
               ),
             ),
             child: const Center(
-              child: Text('Tap To Add Image'),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.photo_library_outlined),
+                  SizedBox(width: 4),
+                  Text(
+                    '사진 추가하기',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: CustomColors.whDarkBlack,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           child: Image(
@@ -253,111 +308,161 @@ class _MyLiveWritingWidgetState extends ConsumerState<MyLiveWritingWidget> {
     TextEditingController contentController,
     ValueNotifier<bool> isLoadingImage,
   ) {
-    return Visibility(
-      visible: !isSubmitted,
-      replacement: ElevatedButton(
-        onPressed: () async {
-          setState(() {
-            isSubmitted = false;
-          });
-          // 다시 편집 상태로 돌아가기
-        },
-        child: const Text('수정'),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    if (isLoadingImage.value) {
+      return const Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          ElevatedButton(
-            onPressed: () async {
-              if (isSubmittable()) {
-                setState(() {
-                  isSubmitted = true;
-                });
-
-                ConfirmPostModel cf = ConfirmPostModel(
-                  title: titleController.text,
-                  content: contentController.text,
-                  resolutionGoalStatement: widget.resolutionModel.goalStatement,
-                  resolutionId: widget.resolutionModel.resolutionId,
-                  imageUrl: imageUrl,
-                  recentStrike: 0,
-                  createdAt: DateTime.now(),
-                  updatedAt: DateTime.now(),
-                  // repository impl에서 자동으로 채워짐
-                  owner: '',
-                  fan: widget.resolutionModel.fanList,
-                  attributes: {
-                    'has_participated_live': true,
-                    'has_rested': true,
-                  },
-                );
-
-                (await ref.read(
-                  createPostUseCaseProvider,
-                )(cf))
-                    .fold(
-                  (l) {
-                    debugPrint(
-                      Failure(l.message).toString(),
-                    );
-                  },
-                  (r) => () {},
-                );
-              } else {
-                // 제출할 수 없음
-                debugPrint(
-                  '제목, 내용, 사진이 모두 입력되지 않아 저장하지 않음',
-                );
-              }
-            },
-            child: Text(isLoadingImage.value ? '처리중' : '휴식'),
+          LinearProgressIndicator(
+            color: CustomColors.whYellow,
+            backgroundColor: CustomColors.whYellowBright,
           ),
-          ElevatedButton(
-            onPressed: () async {
-              if (isSubmittable()) {
-                setState(() {
-                  isSubmitted = true;
-                });
-                ConfirmPostModel cf = ConfirmPostModel(
-                  title: titleController.text,
-                  content: contentController.text,
-                  resolutionGoalStatement: widget.resolutionModel.goalStatement,
-                  resolutionId: widget.resolutionModel.resolutionId,
-                  imageUrl: imageUrl,
-                  recentStrike: 0,
-                  createdAt: DateTime.now(),
-                  updatedAt: DateTime.now(),
-                  owner: '',
-                  fan: widget.resolutionModel.fanList,
-                  attributes: {
-                    'has_participated_live': true,
-                    'has_rested': false,
-                  },
-                );
-
-                (await ref.read(
-                  createPostUseCaseProvider,
-                )(cf))
-                    .fold(
-                  (l) {
-                    debugPrint(
-                      Failure(l.message).toString(),
-                    );
-                  },
-                  (r) => () {},
-                );
-              } else {
-                // 제출할 수 없음
-                debugPrint(
-                  '제목, 내용, 사진이 모두 입력되지 않아 저장하지 않음',
-                );
-              }
-            },
-            child: Text(isLoadingImage.value ? '처리중' : '완료'),
+          SizedBox(height: 5.0),
+          Text(
+            '사진을 공유하고 있습니다',
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w300,
+              color: CustomColors.whWhite,
+            ),
           ),
         ],
-      ),
-    );
+      );
+    } else {
+      return Visibility(
+        visible: !isSubmitted,
+        replacement: ProviderScope(
+          child: ElevatedButton(
+            onPressed: () async {
+              setState(() {
+                isSubmitted = false;
+              });
+              // 다시 편집 상태로 돌아가기
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: CustomColors.whYellow,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              elevation: 15.0,
+            ),
+            child: const Text('수정'),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                if (isSubmittable()) {
+                  setState(() {
+                    isSubmitted = true;
+                  });
+
+                  ConfirmPostModel cf = ConfirmPostModel(
+                    title: titleController.text,
+                    content: contentController.text,
+                    resolutionGoalStatement:
+                        widget.resolutionModel.goalStatement,
+                    resolutionId: widget.resolutionModel.resolutionId,
+                    imageUrl: imageUrl,
+                    recentStrike: 0,
+                    createdAt: DateTime.now(),
+                    updatedAt: DateTime.now(),
+                    // repository impl에서 자동으로 채워짐
+                    owner: '',
+                    fan: widget.resolutionModel.fanList,
+                    attributes: {
+                      'has_participated_live': true,
+                      'has_rested': true,
+                    },
+                  );
+
+                  (await ref.read(
+                    createPostUseCaseProvider,
+                  )(cf))
+                      .fold(
+                    (l) {
+                      debugPrint(
+                        Failure(l.message).toString(),
+                      );
+                    },
+                    (r) => () {},
+                  );
+                } else {
+                  // 제출할 수 없음
+                  debugPrint(
+                    '제목, 내용, 사진이 모두 입력되지 않아 저장하지 않음',
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: imageFile != null
+                    ? CustomColors.whRed
+                    : CustomColors.whGrey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                elevation: 15.0,
+              ),
+              child: const Text('휴식'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (isSubmittable()) {
+                  setState(() {
+                    isSubmitted = true;
+                  });
+                  ConfirmPostModel cf = ConfirmPostModel(
+                    title: titleController.text,
+                    content: contentController.text,
+                    resolutionGoalStatement:
+                        widget.resolutionModel.goalStatement,
+                    resolutionId: widget.resolutionModel.resolutionId,
+                    imageUrl: imageUrl,
+                    recentStrike: 0,
+                    createdAt: DateTime.now(),
+                    updatedAt: DateTime.now(),
+                    owner: '',
+                    fan: widget.resolutionModel.fanList,
+                    attributes: {
+                      'has_participated_live': true,
+                      'has_rested': false,
+                    },
+                  );
+
+                  (await ref.read(
+                    createPostUseCaseProvider,
+                  )(cf))
+                      .fold(
+                    (l) {
+                      debugPrint(
+                        Failure(l.message).toString(),
+                      );
+                    },
+                    (r) => () {},
+                  );
+                } else {
+                  // 제출할 수 없음
+                  debugPrint(
+                    '제목, 내용, 사진이 모두 입력되지 않아 저장하지 않음',
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: imageFile != null
+                    ? CustomColors.whYellow
+                    : CustomColors.whGrey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                elevation: 15.0,
+              ),
+              child: const Text('완료'),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   Future<void> onSave() async {
