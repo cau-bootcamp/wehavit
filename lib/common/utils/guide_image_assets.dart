1d1fe8aa8cc91783870e538c72eb24114d2ef1b1
sm-amoled <moktak072@naver.com>
2024-06-28 17:30:39 +0900

    [#285] asset: 가이드용 이미지 에셋 추가

---
+++ lib/common/utils/guide_image_assets.dart
@@ -0,0 +1,5 @@
+class GuideImage {
+  static const String postGuideImage1 = 'assets/images/guide/post_guide_1.png';
+  static const String postGuideImage2 = 'assets/images/guide/post_guide_2.png';
+  static const String postGuideImage3 = 'assets/images/guide/post_guide_3.png';
+}
