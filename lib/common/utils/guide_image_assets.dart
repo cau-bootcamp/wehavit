6317d902f7be15474a28e89957738706a6c80704
sm-amoled <moktak072@naver.com>
2024-06-28 17:38:35 +0900

    [#285] feat: 글쓰기 가이드 기능 구현

--- lib/common/utils/guide_image_assets.dart
+++ lib/common/utils/guide_image_assets.dart
@@ -2,4 +2,11 @@ class GuideImage {
   static const String postGuideImage1 = 'assets/images/guide/post_guide_1.png';
   static const String postGuideImage2 = 'assets/images/guide/post_guide_2.png';
   static const String postGuideImage3 = 'assets/images/guide/post_guide_3.png';
+
+  static const String reactionGuideImage1 =
+      'assets/images/guide/reaction_guide_1.png';
+  static const String reactionGuideImage2 =
+      'assets/images/guide/reaction_guide_2.png';
+  static const String reactionGuideImage3 =
+      'assets/images/guide/reaction_guide_3.png';
 }
--- lib/presentation/write_post/view/writing_post_guide_view.dart
+++ lib/presentation/write_post/view/writing_post_guide_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:wehavit/common/common.dart';
+import 'package:wehavit/common/utils/guide_image_assets.dart';
 import 'package:wehavit/presentation/presentation.dart';
 
 class WritingPostGuideView extends StatefulWidget {
@@ -46,14 +47,10 @@ class _WritingPostGuideViewState extends State<WritingPostGuideView>
               });
             },
             controller: pageController,
-            children: [
-              const WritingPostGuideFirstView(),
-              Container(
-                color: PointColors.green,
-              ),
-              Container(
-                color: PointColors.pink,
-              ),
+            children: const [
+              WritingPostGuideFirstView(),
+              WritingPostGuideSecondView(),
+              WritingPostGuideThirdView(),
             ],
           ),
         ),
@@ -87,7 +84,13 @@ class WritingPostGuideFirstView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Image.asset(GuideImage.postGuideImage1),
+        ),
+        const SizedBox(height: 24.0),
+        const Text(
+          // ignore: lines_longer_than_80_chars
           '여러분의 각 도전에 대한 인증글을 작성할 수 있어요!\n\n사진은 최대 3장까지 첨부할 수 있고,\n글 없이 사진만 공유해도 괜찮아요!',
           style: TextStyle(
             color: CustomColors.whWhite,
@@ -95,6 +98,90 @@ class WritingPostGuideFirstView extends StatelessWidget {
             fontWeight: FontWeight.w500,
           ),
           textAlign: TextAlign.center,
+        ),
+        Expanded(
+          child: Container(),
+        ),
+        const Text(
+          'Tip',
+          style: TextStyle(
+            color: CustomColors.whWhite,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const Text(
+          '결과보다는 과정이 담긴 인증이 \n서로에게 더 많은 도움이 되어요',
+          style: TextStyle(
+            color: CustomColors.whWhite,
+            fontSize: 13.0,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Expanded(
+          child: Container(),
+        ),
+      ],
+    );
+  }
+}
+
+class WritingPostGuideSecondView extends StatelessWidget {
+  const WritingPostGuideSecondView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Image.asset(GuideImage.postGuideImage2),
+        ),
+        const SizedBox(height: 24.0),
+        const Text(
+          // ignore: lines_longer_than_80_chars
+          '도전을 공유하기로 선택한 친구와 그룹에게만\n각각의 도전에 대한 인증글이 보내집니다!\n\n나머지는 이 인증글을 볼 수 없어요',
+          style: TextStyle(
+            color: CustomColors.whWhite,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Expanded(
+          child: Container(),
+        ),
+      ],
+    );
+  }
+}
+
+class WritingPostGuideThirdView extends StatelessWidget {
+  const WritingPostGuideThirdView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Image.asset(GuideImage.postGuideImage3),
+        ),
+        const SizedBox(height: 24.0),
+        const Text(
+          // ignore: lines_longer_than_80_chars
+          '글쓰기가 귀찮다면? \n그냥 완료 표시만 남겨주세요!\n\n혹시 오늘 목표를 실천하지 못했더라도\n반성을 적으면 친구들에게 격려를 받을 수 있어요!',
+          style: TextStyle(
+            color: CustomColors.whWhite,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Expanded(
+          child: Container(),
         ),
       ],
     );
