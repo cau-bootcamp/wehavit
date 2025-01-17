759e7d92a60117d1219746326dabe672cb029ac7
sm-amoled <moktak072@naver.com>
2025-01-17 10:49:55 +0900

    [#359] feat: userDataEntityProvider 추가

--- lib/presentation/state/friend/friend_list_provider.dart
+++ lib/presentation/state/friend/friend_list_provider.dart
@@ -2,6 +2,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wehavit/dependency/domain/usecase_dependency.dart';
 import 'package:wehavit/domain/entities/entities.dart';
 
+final friendUidListProvider = FutureProvider<List<String>>(
+  (ref) => ref.read(getFriendUidListUseCaseProvider).call().then(
+        (result) => result.fold(
+          (failure) => [],
+          (success) => success,
+        ),
+      ),
+);
+
+final userDataEntityProvider = FutureProvider.family<UserDataEntity, String>(
+  (ref, userId) => ref.read(getUserDataFromIdUsecaseProvider).call(userId).then(
+        (result) => result.fold(
+          (failure) => Future.error(failure.message),
+          (success) => success,
+        ),
+      ),
+);
+
 final friendListProvider = FutureProvider<List<UserDataEntity>>(
   (ref) => ref.read(getFriendListUseCaseProvider).call().then(
         (result) => result.fold(
